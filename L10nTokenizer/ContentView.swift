4b67dc23d2f37fd5ba62398e7ea4ead4b025ae90
George Kaimakas <gkaimakas@gmail.com>
2024-01-05 14:46:09 +0200

    Initial Commit

---
+++ L10nTokenizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L10nTokenizer
+//
+//  Created by George Kaimakas on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ L10nTokenizer/L10nTokenizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  L10nTokenizerApp.swift
+//  L10nTokenizer
+//
+//  Created by George Kaimakas on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct L10nTokenizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
